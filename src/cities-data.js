37b2c3c0ee5ba4a7a37fc9776b7b926e30a8a6a3
Will Gundy <willgunderson503@gmail.com>
2022-05-11 18:25:44 -0700

    initial commit

---
+++ src/cities-data.js
@@ -0,0 +1,9 @@
+export const cities = [
+  { name: 'New York City', pop: 8804190, area: '300 sq mi', popdense: '29,298/sq mi' },
+  { name: 'Los Angeles', pop: 3898747, area: '470 sq mi', popdense: '8,304/sq mi' },
+  { name: 'Chicago', pop: 2746388, area: '228 sq mi', popdense: '12,061/sq mi' },
+  { name: 'Houston', pop: 2304580, area: '640 sq mi', popdense: '3,600/sq mi' },
+  { name: 'Phoenix', pop: 1608139, area: '518 sq mi', popdense: '3,105/sq mi' },
+  { name: 'Philadelphia', pop: 1603797, area: '134 sq mi', popdense: '11,933/sq mi' },
+];
+  
